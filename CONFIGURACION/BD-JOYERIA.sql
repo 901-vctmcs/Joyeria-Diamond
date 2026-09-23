e6978f916a2673c5ab91254dbf0414d33a460caa
Victor <vctrestrada88@gmail.com>
2026-09-23 09:56:14 -0600

    Se modifica BD

--- CONFIGURACION/BD-JOYERIA.sql
+++ CONFIGURACION/BD-JOYERIA.sql
@@ -1,1 +1,10 @@
-CREATE BD JOYERIA
+create database BD_JOYERIA;
+use BD_JOYERIA;
+create table CLIENTES(
+    ID_CLIENTE int primary key auto_increment,
+    NOMBRE varchar(50) not null,
+    APELLIDO varchar(50) not null,
+    TELEFONO varchar(15) not null,
+    CORREO varchar(50) not null
+);
+
